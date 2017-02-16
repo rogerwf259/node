85da419f38e7dd2871e01291d18383b7b3f8a9a1
Daniel <rogeliomer_2@hotmail.com>
2017-02-15 22:01:21 -0600

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,44 +2,39 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
-
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
+
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to append File');
-    }
-  });
+  fs.appendFile('server.log', log + '\n');
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerPartials(__dirname + '/views/partials');
-
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear()
+  return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
-app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello Express!</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    message: 'Welcome to the home page'
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
@@ -56,6 +51,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
